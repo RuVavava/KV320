bbd93d96536e0c952318180a87f90f06b753ffad
User03 <varya.kornilova.21@inbox.ru>
2024-07-04 10:14:02 +0300

    --

---
+++ KV320/Pages/ProductListLayoutPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KV320.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListLayoutPage.xaml
+    /// </summary>
+    public partial class ProductListLayoutPage : Page
+    {
+        public ProductListLayoutPage()
+        {
+            InitializeComponent();
+            List<int> list = new List<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+
+            productLV.ItemsSource = list;
+        }
+    }
+}
